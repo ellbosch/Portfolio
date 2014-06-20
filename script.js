b91a426f17a5c25d5a89c06913992f8fca21317f
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2014-06-20 17:30:32 -0400

    scroll animations (not fixed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var didScroll = false;
+
 	// bootstrap scroll-spy
 	$("body").scrollspy({ target: "#navbar" });
 	$('[data-spy="scroll"]').each(function () {
@@ -13,26 +15,60 @@ $(document).ready(function() {
 
 	// make sure all nav elements are deactive
 	$(window).scroll(function() {
-		// fade to next page if at beginning
-		if ($(document).scrollTop() < $("#cover-page").height()) {
-			$(window)
-			$(".container").fadeIn("slow");
-		} 
-		if ($("#cover-page").is(':hidden') && $(document).scrollTop() == 0) {
-			//$("#cover-page").fadeIn("slow");
-		}
+		didScroll = true;
+		// scroll to next page if at beginning
+		// if ($(document).scrollTop() < ($("#cover-page").height() / 2)) {
+		// 	$('html, body').animate({
+		// 		scrollTop: $("#about").offset().top
+		// 	}, 1000);
+		// }
 
-		if ($(document).scrollTop() < 100) {
-  			$("#aboutTab a").css('color', '#EEEEEE');
-  		} else {
-  			$("#aboutTab.active > a").css('color', 'red');
-  		}
+		// if ($(document).scrollTop() < 100) {
+  // 			$("#aboutTab a").css('color', '#EEEEEE');
+  // 		} else {
+  // 			$("#aboutTab.active > a").css('color', 'red');
+  // 		}
 	});
 
+	// set delay to performing actions involving a scroll event
+	setInterval(function() {
+		var posTop = $(document).scrollTop();
+
+		if (didScroll) {
+			didScroll = false;
+
+			// animate to about div
+			if (posTop < ($("#cover-page").height() / 4)) {
+				$('html, body').animate({
+					scrollTop: $("#about").offset().top
+				}, 1000).delay(800);
+			// animate to top
+			} else if (posTop < $("#cover-page").height() * 0.75 &&
+				posTop > ($("cover-page").height() / 4)) {
+				$('html, body').animate({
+					scrollTop: 0
+				}, 1000);
+			}
+
+/****************************************************
+	note to self: make rubber band effect with jquery animations by
+	animating window down when window doesn't scroll to certain height
+*********************************************************/
+
+			// HANDLE COLOR CHANGE WHEN ANIMATIONS HAPPEN
+			if ($(document).scrollTop() < 100) {
+	  			$("#aboutTab a").css('color', '#EEEEEE');
+	  		}
+		}
+	}, 250);
+
 	// parallelx animations, dawg
-	var parOffset = 49;
-	$.stellar(/*{
-		horizontalOffset: parOffset,
-		verticalOffset: parOffset
-	}*/);
+	$.stellar();
+
+	// fix position of scroll div when window resizes
+	$(window).resize(function() {
+		$("#scroll-button").css('left','50%');
+		$("#scroll-button").css('right','50%');
+	});
+
 });
